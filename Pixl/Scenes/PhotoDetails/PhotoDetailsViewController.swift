3dc6030d84215f2168b8730bcf6c383cd7c75ed4
dscyrescotti <dscyrescotti@gmail.com>
2021-04-23 23:09:50 +0630

    feat: add support for multiple window

---
+++ Pixl/Scenes/PhotoDetails/PhotoDetailsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoDetailsViewController.swift
+//  Pixl
+//
+//  Created by Dscyre Scotti on 23/04/2021.
+//
+
+import UIKit
+
+class PhotoDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
